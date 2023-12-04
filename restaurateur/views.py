f4781a9f768fe477b29d5c2dea406e153cd6b805
Anton Ozerov <lamerork@gmail.com>
2023-12-04 12:23:17 +0300

    Added distance

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -1,13 +1,17 @@
+import requests
 from django import forms
 from django.shortcuts import redirect, render
 from django.views import View
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
+from django.conf import settings
 
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
 
-from foodcartapp.models import Product, Restaurant, Order, RestaurantMenuItem
+from foodcartapp.models import Product, Restaurant, Order
+
+from geopy import distance
 
 
 class Login(forms.Form):
@@ -89,6 +93,24 @@ def view_restaurants(request):
     })
 
 
+def fetch_coordinates(apikey, address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": apikey,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lon, lat
+
+
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
 
@@ -96,6 +118,16 @@ def view_orders(request):
 
     for order_item in order_items:
         order_item.restaurants = Order.objects.filter_restaurants_for_order(order_item.id)
+        order_coordinates = fetch_coordinates(settings.YANDEX_MAP_API, order_item.address)
+        for restauran in order_item.restaurants:
+            restauran_coordinates = fetch_coordinates(settings.YANDEX_MAP_API, restauran.address)
+            
+            if order_coordinates and restauran_coordinates:
+                restauran.distance = round(distance.distance(order_coordinates, restauran_coordinates).kilometers, 2)
+            else:
+                restauran.distance = 'Ошибка определения координат'
+
+        order_item.restaurants = sorted(order_item.restaurants, key=lambda x: x.distance)
 
     return render(request, template_name='order_items.html', context={
         'order_items': order_items,
--- star_burger/settings.py
+++ star_burger/settings.py
@@ -124,3 +124,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"),
     os.path.join(BASE_DIR, "bundles"),
 ]
+
+YANDEX_MAP_API = env.str('YANDEX_MAP_API')
