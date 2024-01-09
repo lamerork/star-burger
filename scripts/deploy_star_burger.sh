960ee5a664dd15fd6ef428de79920ceb8ac2f8dd
Anton Ozerov <lamerork@gmail.com>
2024-01-09 13:11:46 +0300

    update deploy script

--- scripts/deploy_star_burger.sh
+++ scripts/deploy_star_burger.sh
@@ -8,7 +8,7 @@ echo 'git pull'
 git pull
 
 echo 'migrate'
-/opt/star-burger/env/bin/python manage.py migrate
+/opt/star-burger/env/bin/python /opt/star-burger/manage.py migrate
 
 echo 'restart star-burger daemon'
 systemctl restart star-burger
